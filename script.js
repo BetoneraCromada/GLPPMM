ee3ff4d248a5328a6bb4826c9c8e1f6f503f9390
BetoneraCromada <grpereira037@gmail.com>
2024-11-18 01:12:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+// Corrigido para getElementById (singular)
+const starContainer = document.getElementById('stars');
+const starCount = 200;
+
+for(let i = 0; i < starCount; i++) {
+    const star = document.createElement('div');
+    star.className = 'star';
+    star.style.top = Math.random() * 100 + '%';
+    star.style.left = Math.random() * 100 + '%';
+    star.style.animationDelay = Math.random() * 5 + 's';
+    starContainer.appendChild(star);
+}
